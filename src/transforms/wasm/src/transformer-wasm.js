cbb65fde11b8b8cfc6491abc81a6e7f5544c9d59
Jamie Kyle <me@thejameskyle.com>
2018-06-28 13:42:29 -0700

    fill out files

---
+++ src/transforms/wasm/src/transformer-wasm.js
@@ -0,0 +1,20 @@
+// @flow
+import { Transform } from '@parcel/plugin';
+
+export default new Transform({
+  async parse() {
+    // ...
+  },
+
+  async dependencies() {
+    // ...
+  },
+
+  async transform() {
+    // ...
+  },
+
+  async generate() {
+    // ...
+  },
+});
